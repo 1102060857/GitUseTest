abba6c8cab27ece2057354db464e090c61dd207e
yuqing.song <1102060857@qq.com>
2023-09-26 09:45:50 +0800

    修改dev01

--- app/src/main/java/com/android/gitusetest/MainActivity.kt
+++ app/src/main/java/com/android/gitusetest/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android Hello Ktlion")
+                    Greeting("Android Hello Ktlion这是dev01分支")
                 }
             }
         }
